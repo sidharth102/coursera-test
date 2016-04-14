7078ee5f3faf6d6383464dffe48189169a0364a5
sidharth102 <sidharth102@yahoo.co.in>
2016-04-14 22:00:48 +0530

    changes

--- mod4-sol/js/script.js
+++ mod4-sol/js/script.js
@@ -98,7 +98,7 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
 
-     var chosenCategoryShortName = chooseRandomCategory(categories).short_name;               // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
+     var chosenCategoryShortName = chooseRandomCategory(categories);               // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
